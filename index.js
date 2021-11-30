a73636534c806d83affecd703d785410052a98b9
MasudRana924 <masud15-924@diu.edu.bd>
2021-11-30 02:56:36 -0600

    done

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+// middleware 
+app.use(cors())
+app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrpwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+async function run() {
+    try {
+        await client.connect() 
+        console.log('database connected')
+        const database = client.db('portfolioDB')
+        // projects connection
+        const projectsCollection = database.collection('projects')
+
+        app.get('/projects', async (req, res) => {
+            const cursor = projectsCollection.find({})
+            const projects = await cursor.toArray()
+            res.send(projects)
+
+        })
+        app.get('/projects/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const projects = await projectsCollection.findOne(query)
+            res.json(projects)
+        })
+      
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+    res.send('eyes_Shades is running under server')
+})
+app.listen(port, () => {
+    console.log("server is running on", port)
+})
